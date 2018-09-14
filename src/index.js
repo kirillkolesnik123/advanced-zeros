d75c4e7f94f934fc8b9f1e039dd2c44a1eec30ce
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-09-14 11:49:33 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+   var times;
+    var sum_of_zer = number;
+    for (var i = 2; i <= base;i++){
+      if (base % i == 0){ //ищем кратную цифру
+        times = 0;
+        while (base % i == 0){
+          times++;// количество кратных проходов
+          base = Math.floor(base / i);
+        }
+        var zer_count = 0;
+        sub_number = number;
+
+        while (Math.floor(sub_number / i) > 0){// делим число и записываем в сумму частных в zer_count
+          zer_count += Math.floor(sub_number / i);
+          sub_number = Math.floor(sub_number / i);
+        }
+        sum_of_zer = Math.min (sum_of_zer , Math.floor(zer_count / times)); // меньшее из данного прохода и предыдушего 'i'
+      }
+    }
+    return sum_of_zer;
+  
 }
